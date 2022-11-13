6fde68e21eaab9df29aa903e90283f568c6447e2
Bernat Bellmunt <bbellmunt@gmail.com>
2022-11-13 13:56:29 +0100

    sunday morning checkpoint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import random
+import operator
 import numpy as np
 import markdown.extensions.fenced_code
 import tools.sql_queries as esecuele
@@ -9,25 +10,44 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 sia = SentimentIntensityAnalyzer()
 
 
+
+from wordcloud import WordCloud
+from langdetect import detect
+from textblob import TextBlob
+
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+
+
+
 app = Flask(__name__)
 
-# Render the markdwon
+# Render the markdown - OK
 @app.route("/")
 def readme ():
     readme_file = open("README.md", "r")
     return markdown.markdown(readme_file.read(), extensions = ["fenced_code"])
 
 # GET ENDPOINTS: SQL 
-# SQL get everything
-@app.route("/sql/")
+# SQL get everything - OK
+@app.route("/everything/")
 def sql ():
     return jsonify(esecuele.get_everything())
 
-@app.route("/sql/<name>", )
+"""# I WANT TO GET THE SEASON_NO, EPISODE_NO AND EPISODE_NAME FROM MY DB
+@app.route("/episodes")
+def episodes ():
+    return jsonify(esecuele.return_episodes_and_seasons())"""
+
+
+# return all details from a Character -OK
+@app.route("/details/<name>", )
 def lines_from_characters (name):
     return jsonify(esecuele.get_everything_from_character(name))
 
-#all lines of a character-> we pass sentiment
+#sentiment of each line - OK
 @app.route("/sa/<name>/", )
 def sa_from_character (name):
     everything = esecuele.get_just_dialogue(name)
@@ -35,7 +55,7 @@ def sa_from_character (name):
     list_sentiment = [sia.polarity_scores(i["line"])["compound"] for i in everything]
     return jsonify(list_sentiment)
 
-#avg sentiment all lines of a character-
+#avg sentiment all lines of a character- OK
 @app.route("/avg_sa/<name>/", )
 def avg_sa_from_character (name):
     everything = esecuele.get_just_dialogue(name)
@@ -43,14 +63,35 @@ def avg_sa_from_character (name):
     list_sentiment = [sia.polarity_scores(i["line"])["compound"] for i in everything]
     return jsonify(sum(list_sentiment)/len(list_sentiment))
 
-#avg sentiment of all characters lines of a character
-"""@app.route("/sa/all_characters", )
+#avg sentiment of all characters lines of a character - OK
+@app.route("/all_sentiments", )
 def all_sa ():
-    everything = esecuele.get_lines_from_all()
-    for """
+    everything = esecuele.get_lines_from_char()
+    new_d = {}
+    for k, v in everything.items():
+        list_sentiment = [sia.polarity_scores(i)["compound"] for i in v]
+        avg_sent = sum(list_sentiment)/len(list_sentiment)
+        new_d[k]=avg_sent
+    return new_d
 
+#word cloud per character - does not work - processing a lot and 
+"""@app.route("/wordcloud/<name>", )
+def word_cloud_charac (name):
+    everything = esecuele.get_lines_from_char()
+    lines = everything[name]
+    st=""
+    for dialog in lines:
+        st+=dialog
+    wordcloud = WordCloud().generate(st)
+    plt.figure(figsize=(10,10), facecolor="k")
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+    #plt.savefig('images/wordcloud.png', facecolor='k', bbox_inches='tight')
+    plt.show();
+    return wordcloud"""
 
-####### POST
+####### POST -- DOES NOT WORK - METHOD NOT ALLOWED
 @app.route("/insertrow", methods=["POST"])
 def try_post ():
     # Decoding params
--- tools/sql_queries.py
+++ tools/sql_queries.py
@@ -24,12 +24,36 @@ def get_just_dialogue (name):
 
 def get_lines_from_all ():
     query = """SELECT character_name, line
-    from all_info
+    from all_info;
     """
     df = pd.read_sql_query(query, engine)
     return df.to_dict(orient="records")
 
+def get_lines_from_char ():
+    query = """SELECT character_name, line
+    from all_info;
+    """
+    df = pd.read_sql_query(query, engine)
+    list_all_lines = df.to_dict(orient="records")
+    
+    lines_dict={}
+    for line in list_all_lines:
+        char = line["character_name"]
+        diu = line["line"]
+        if char not in lines_dict:
+            list_lines=[]
+            list_lines.append(diu)
+            lines_dict[char]=list_lines
+        else:
+            lines_dict[char].append(diu)
+    return lines_dict
 
+'''def return_episodes_and_seasons(): -- # NO FUNCIONA COM VULL
+    query = """SELECT season_no, episode_no, DISTINCT(episode_name)
+    from all_info
+    """
+    df = pd.read_sql_query(query, engine)
+    return df.to_dict(orient="records")'''
 
 def insert_one_row (season_no, episode_no, episode_name, character_name, line):
     query = f"""INSERT INTO all_info
